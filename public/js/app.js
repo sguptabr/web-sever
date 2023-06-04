f73ec59073fa88869dd1ed6dbe4f465836a72e91
Sanjay Gupta BR <sguptabr@gmail.com>
2023-06-04 14:03:32 +0530

    Removed unneccesary console.log command

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-console.log('From an static web page')
-
-// fetch('https://puzzle.mead.io/puzzle').then( (response) => {
-//     response.json().then((data) =>{
-//         console.log(data)
-//     })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const message1 = document.querySelector('#messageOne')
